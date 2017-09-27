7c171d6b00835ce43ff49b15db2b1f12225f3c2a
naorone <naorone@gmail.com>
2017-09-27 15:06:49 +0200

    Cambio Pedro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Pedro
                 </div>
 
                 <div class="links">
